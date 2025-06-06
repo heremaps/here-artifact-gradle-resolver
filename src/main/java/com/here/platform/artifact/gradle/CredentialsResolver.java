eb5a1fd5f705b528b4c9eac88a5ffa041f739fc8
Oleksandr Vyshniak <ext-oleksandr.vyshniak@here.com>
2025-06-06 09:43:30 +0300

    Add logging and credentials validation
    Signed-off-by: Oleksandr Vyshniak <ext-oleksandr.vyshniak@here.com>

--- src/main/java/com/here/platform/artifact/gradle/CredentialsResolver.java
+++ src/main/java/com/here/platform/artifact/gradle/CredentialsResolver.java
@@ -18,6 +18,9 @@
  */
 package com.here.platform.artifact.gradle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.util.Properties;
 
@@ -25,7 +28,6 @@ import static org.apache.http.util.TextUtils.isEmpty;
 
 /**
  * Resolves credentials based on system configuration. Credentials precedence:
- * <p>
  * <ol>
  * <li>-DhereCredentialsFile system property
  * <li>HERE_CREDENTIALS_FILE environment variable
@@ -35,6 +37,13 @@ import static org.apache.http.util.TextUtils.isEmpty;
  */
 public class CredentialsResolver {
 
+    public static final String ACCESS_KEY_ID = "here.access.key.id";
+    public static final String ACCESS_KEY_SECRET = "here.access.key.secret";
+    public static final String TOKEN_ENDPOINT_URL = "here.token.endpoint.url";
+    public static final String CLIENT_ID = "here.client.id";
+
+    private static final Logger LOG = LoggerFactory.getLogger(CredentialsResolver.class);
+
     private static final String HERE_CREDENTIALS_PROPERTY = "hereCredentialsFile";
     private static final String HERE_CREDENTIALS_STRING_ENV = "HERE_CREDENTIALS_STRING";
     private static final String HERE_CREDENTIALS_PATH = ".here/credentials.properties";
@@ -42,7 +51,6 @@ public class CredentialsResolver {
 
     /**
      * Resolve credentials based on a precedence:
-     * <p>
      * <ol>
      * <li>-DhereCredentialsFile system property
      * <li>HERE_CREDENTIALS_FILE environment variable
@@ -64,14 +72,24 @@ public class CredentialsResolver {
         } else {
             loadCredentialsFromFile(properties, new File(System.getProperty("user.home"), HERE_CREDENTIALS_PATH));
         }
+        validateCredentials(properties);
         return properties;
     }
 
     private File resolveFile() {
         File file = null;
         String systemPropertyFile = System.getProperty(HERE_CREDENTIALS_PROPERTY);
-        if (isEmpty(systemPropertyFile)) {
+        if (!isEmpty(systemPropertyFile)) {
+            LOG
+                .debug("Found property file value at System Property {}: {}", HERE_CREDENTIALS_PROPERTY,
+                    systemPropertyFile);
+        } else {
             systemPropertyFile = System.getenv(HERE_CREDENTIALS_ENV);
+            if (!isEmpty(systemPropertyFile)) {
+                LOG
+                    .debug("Found property file at Environment Property {}: {}", HERE_CREDENTIALS_ENV,
+                        systemPropertyFile);
+            }
         }
         if (!isEmpty(systemPropertyFile)) {
             file = new File(systemPropertyFile);
@@ -80,22 +98,44 @@ public class CredentialsResolver {
     }
 
     private void loadCredentialsFromFile(Properties properties, File file) {
+        LOG.debug("Using here credentials file: {}", file.getAbsolutePath());
         if (file.exists() && file.canRead()) {
+            LOG.debug("Attempting to read credentials file at: {}", file.getAbsolutePath());
             try (InputStream in = new FileInputStream(file)) {
                 properties.load(in);
             } catch (IOException exp) {
+                LOG.error("Unable to read client credentials at {}", file.getAbsolutePath(), exp);
                 throw new RuntimeException("Unable to read client credentials at " + file.getAbsolutePath(), exp);
             }
+        } else {
+            LOG.warn("Unable to read configured file: {}", file.getAbsolutePath());
         }
     }
 
     private void loadCredentialsFromString(Properties properties, String credentialsString) {
+        LOG.debug("Attempting to create credentials from environment variable");
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(credentialsString.getBytes());
         try {
             properties.load(byteArrayInputStream);
         } catch (IOException exp) {
+            LOG
+                .error("Unable to create client credentials from environment variable {}", HERE_CREDENTIALS_STRING_ENV,
+                    exp);
             throw new RuntimeException(
                 "Unable to create client credentials from environment variable " + HERE_CREDENTIALS_STRING_ENV, exp);
+        }
+    }
+
+    void validateCredentials(Properties properties) {
+        propertyMustExists(properties, ACCESS_KEY_ID);
+        propertyMustExists(properties, ACCESS_KEY_SECRET);
+        propertyMustExists(properties, TOKEN_ENDPOINT_URL);
+        propertyMustExists(properties, CLIENT_ID);
+    }
+
+    private void propertyMustExists(Properties properties, String propertyName) {
+        if (properties.getProperty(propertyName) == null) {
+            throw new RuntimeException("Credentials don't contain the property: " + propertyName);
         }
     }
 }
